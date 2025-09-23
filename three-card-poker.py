9f6b44ee61199a444e527acfa85bd1f33a2557a6
ryder <ryderortiz7@gmail.com>
2025-09-23 11:43:38 -0400

    sort_hand function

--- three-card-poker.py
+++ three-card-poker.py
@@ -25,6 +25,30 @@ def hand_to_string(hand: list[int]) -> str:
             result.append(face_cards[card])
     return " ".join(result)
 
+# tests
 assert_equal(hand_to_string([2, 3, 4]), "2 3 4")
 assert_equal(hand_to_string([11, 12, 13]), "J Q K")
 assert_equal(hand_to_string([10, 7, 14]), "X 7 A")
+
+def sort_hand(hand: list[int]) -> list[int]:
+    """
+    Sorts the numbers in the user's hand of cards.
+    
+    Args:
+        hand (list[int]): The original hand of cards
+    Returns:
+        list[int]: The sorted list of cards
+    """
+    copy = hand
+    result = [0] * 3
+    result[0] = max(hand)
+    copy.remove(max(hand))
+    result[2] = min(hand)
+    copy.remove(min(hand))
+    result[1] = copy[0]
+    return result
+
+# tests
+assert_equal(sort_hand([2, 4, 3]), [4, 3, 2])
+assert_equal(sort_hand([12, 6, 3]), [12, 6, 3])
+assert_equal(sort_hand([7, 8, 9]), [9, 8, 7])
