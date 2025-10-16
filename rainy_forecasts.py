b34ac573b813e1c87427b37ba237ca52c9e74a3a
ryder <ryderortiz7@gmail.com>
2025-10-16 09:35:45 -0400

    completed exercise

--- rainy_forecasts.py
+++ rainy_forecasts.py
@@ -31,7 +31,33 @@ def forecasts_to_reports(forecasts: list[Forecast]) -> list[Report]:
             reports.append(report)
     return reports
 
-# def max_rainfall()
-
 def rainiest_cloudy(forecasts: list[Forecast]) -> bool:
     reports = forecasts_to_reports(forecasts)
+    if not reports:
+        return False
+
+    day_most_rainfall: Report = reports[0]
+    max_rainfall: int = day_most_rainfall.rainfall[0].amount
+
+    for report in reports:
+        report_total = 0
+        for rain in report.rainfall:
+            report_total += rain.amount
+        if report_total > max_rainfall:
+            max_rainfall = report_total
+            day_most_rainfall = report
+    
+    return day_most_rainfall.weather.cloudy
+
+rainfall1 = [Measurement(2, True), Measurement(5, False)]
+rainfall2 = [Measurement(8, True), Measurement(6, False)]
+report1 = Report(32, rainfall1, WeatherOptions(True, False, True))
+report2 = Report(44, rainfall2, WeatherOptions(True, True, True))
+report3 = Report(50, rainfall1, WeatherOptions(True, True, True))
+forecasts1 = [Forecast("w","w", [report1, report2]), Forecast("h","h", [report2, report1])]
+forecasts2 = [Forecast("h","h", [report3, report2]), Forecast("w","w", [report2, report2])]
+forecasts3 = [Forecast("h","h", []), Forecast("w","w", [])]
+
+assert_equal(rainiest_cloudy(forecasts1), True)
+assert_equal(rainiest_cloudy(forecasts2), True)
+assert_equal(rainiest_cloudy(forecasts3), False)
