dbb39d7777a07924e1ae8591fafa3f2f0a8ed06c
ryder <ryderortiz7@gmail.com>
2025-10-23 15:25:47 -0400

    main function

--- crypto_corgi.py
+++ crypto_corgi.py
@@ -9,8 +9,8 @@ def rotate_values(original_values: list[int], rotation_amount: int) -> list[int]
     Rotates a list of values according to the rotation formula.
 
     Args:
-        original_values(list[int]): The list of integer values corresponding to ASCII characters.
-        rotation_amount(int): The amount for each character to be rotated.
+        original_values (list[int]): The list of integer values corresponding to ASCII characters.
+        rotation_amount (int): The amount for each character to be rotated.
     Returns:
         list[int]: A new list of the rotated values that correspond to the original values.
     """
@@ -24,7 +24,7 @@ def encrypt_text(message: str, rotation_amount: int) -> str:
     Encrypts inputted text using the Caesar Cipher.
 
     Args:
-        message: Text to be encrypted
+        message (str): Text to be encrypted
         rotation_amount(int): The amount for each character to be rotated.
     Returns:
         str: Encrypted text according to the rotation amount
@@ -44,7 +44,7 @@ def decrypt_text(message: str, rotation_amount: int) -> str:
     Decrypts inputted text inversing the Caesar Cipher.
 
     Args:
-        message: Text to be decrypted
+        message (str): Text to be decrypted
         rotation_amount(int): The amount for each character to be rotated.
     Returns:
         str: Decrypted text according to the rotation amount
@@ -85,3 +85,32 @@ def hash_text(message: str, base: int, hash_size: int) -> int:
 assert_equal(hash_text("A", 11, 100), 51)
 assert_equal(hash_text("AB", 11, 100), 35)
 assert_equal(hash_text("ABC", 11, 100), 52)
+
+# 4) Define main
+def main():
+    """
+    Main function to run the encryption/decryption program.
+    
+    Args:
+        None
+    Returns:
+        None
+    """
+    action = input("Enter your desired action. (encrypt/decrypt).")
+    if action == "encrypt":
+        message = input("Enter your plaintext message.")
+        encrypted_message = encrypt_text(message, 23)
+        hashed_value = hash_text(message, 31, 1000000000)
+        print(f"Your encrypted message is {encrypted_message}, and your hashed value is {hashed_value}.")
+    elif action == "decrypt":
+        message = input("Enter your encrypted message.")
+        decrypted_message = decrypt_text(message, 23)
+        expected_hash = input("Enter the expected hash.")
+        if int(expected_hash) == hash_text(message, 31, 1000000000):
+            print(f"Your decrypted message is {decrypted_message}.")
+        else:
+            print("error: hashes don't match")
+    else:
+        print("Please enter a valid action.")
+
+main()
